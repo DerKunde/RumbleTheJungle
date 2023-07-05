cef681d514c7bcbf1ab6472067fec0686a1b9f65
Sean <seanrindl@gmail.com>
2023-07-05 20:08:51 +0200

    Fixed Merge Issues

--- Assets/Scenes/UI/healthbar.cs
+++ Assets/Scenes/UI/healthbar.cs
@@ -40,33 +40,4 @@ public class Healthbar : MonoBehaviour
     {
         
     }
-
-    public Image[] hearts;
-    public Sprite fullHeart;
-    public Sprite halfHeart;
-    public Sprite emptyHeart;
-
-    void Update() {
-
-        if(health > numberOfHearts) {
-            health = numberOfHearts;
-        }
-
-        for (int i = 0; i < hearts.Length; i++)
-        {
-            if(i < health) {
-                hearts[i].sprite = fullHeart;
-            } else {
-                hearts[i].sprite = emptyHeart;
-            }
-
-            if (i < numberOfHearts)
-            {
-                hearts[i].enabled = true;
-            } else
-            {
-                hearts[i].enabled = false;
-            }
-        }
-    }
 }
